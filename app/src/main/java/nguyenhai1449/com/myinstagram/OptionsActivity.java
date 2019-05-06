98605570ee474c2380115c3901a82922205f5739
NguyenHai1449 <nguyenhai1449@gmail.com>
2019-05-06 08:26:09 +0700

    Hoàn tất đồ án

--- app/src/main/java/nguyenhai1449/com/myinstagram/OptionsActivity.java
+++ app/src/main/java/nguyenhai1449/com/myinstagram/OptionsActivity.java
@@ -1,6 +1,7 @@
 package nguyenhai1449.com.myinstagram;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +13,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class OptionsActivity extends AppCompatActivity {
 
     TextView logout;
+    TextView about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,14 @@ public class OptionsActivity extends AppCompatActivity {
             }
         });
         logout = findViewById(R.id.logout);
+        about = findViewById(R.id.about);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                about();
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +48,13 @@ public class OptionsActivity extends AppCompatActivity {
                         .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
+    }
+    private void about(){
+        AlertDialog.Builder a = new AlertDialog.Builder(this);
+        a.setTitle(R.string.about);
+        a.setMessage("MyInstagram - Version 1.0" + "\n" + "\n" +
+                "Ứng Dụng Chia Sẻ Hình Ảnh "+ "\n" + "\n" +
+                "Thiết kế và Phát Triển Bởi Nhóm VI");
+        a.show();
     }
 }
